c74bdb53ea85eb814dc9d9f784cbb6234dc88b9f
Ignacio Rigoni <ignacio.rigoni@gmail.com>
2021-11-15 11:38:22 -0300

    query parameters

--- main_2.py
+++ main_2.py
@@ -51,3 +51,14 @@ async def read_file(file_path: str, item: int):
     return {
         "file_path": file_path, "item": item
     }
+
+
+# Query Parameters
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip:skip + limit]
+
+
